d79fed7b35639e3fccc9255e98d52587b5efb968
Your Name <you@example.com>
2026-05-27 20:25:30 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/impossible-game/',
+<<<<<<< HEAD
 })
+=======
+})
+>>>>>>> 5bd5a23 (initial commit)
